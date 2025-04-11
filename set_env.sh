a0d8e7786fd787cc821c4bd15d9ba4b1c448b75d
Sajal Dash <62o@login1.odo.olcf.ornl.gov>
2025-04-11 11:23:04 -0400

    Added environment for finetuning.

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load rocm/6.1.3
+module load gcc/12.2.0
+module load miniforge3
+
+#source /gpfs/wolf2/olcf/trn040/world-shared/sajal/miniconda3/bin/activate
+conda activate /gpfs/wolf2/olcf/trn040/world-shared/sajal/env-ft-6.1.3
+
+
+
