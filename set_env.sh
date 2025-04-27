a144d9498d910d1a319e8a950e1f44b1b8b32a6e
Sajal Dash <62o@odo02.odo.olcf.ornl.gov>
2025-04-27 11:22:43 -0400

    Added DeepSpeed Multi-GPU support

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,13 @@
 
 module load rocm/6.1.3
 module load gcc/12.2.0
-module load miniforge3
+module load miniforge3/23.11.0
+
+export ROCM_HOME=/opt/rocm-6.1.3
 
 #source /gpfs/wolf2/olcf/trn040/world-shared/sajal/miniconda3/bin/activate
-conda activate /gpfs/wolf2/olcf/trn040/world-shared/sajal/env-ft-6.1.3
-
+conda activate /gpfs/wolf2/olcf/trn040/world-shared/sajal/env-ft-6.1.3-ro
+module unload miniforge3/23.11.0
+export HF_HUB_OFFLINE=1
 
 
