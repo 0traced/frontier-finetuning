10384c246ad84d69695c572cd28e5320a3a0fd53
Sajal Dash <62o@login2.odo.olcf.ornl.gov>
2025-04-28 13:50:56 -0400

    Added a readonly conda environment for class to use.

--- set_env.sh
+++ set_env.sh
@@ -9,6 +9,6 @@ export ROCM_HOME=/opt/rocm-6.1.3
 #source /gpfs/wolf2/olcf/trn040/world-shared/sajal/miniconda3/bin/activate
 conda activate /gpfs/wolf2/olcf/trn040/world-shared/sajal/env-ft-6.1.3-ro
 module unload miniforge3/23.11.0
-export HF_HUB_OFFLINE=1
+export HF_HUB_OFFLINE=0
 
 
